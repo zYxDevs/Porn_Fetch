84c90ea816dc4d592adf0005e64b37409f898977
Johannes Habel <EchterAlsFake@proton.me>
2023-09-19 21:16:26 +0200

    Import worked now.  Trying first download.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.progressbar import ProgressBar
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 from threading import Thread
+from plyer import filechooser
 try:
     from phub import Client, Quality
 
@@ -34,20 +38,32 @@ class MyBoxLayout(BoxLayout):
         self.progress_bar = ProgressBar(max=100)  # Progress bar with maximum value of 100
         self.add_widget(self.progress_bar)
 
+    def show_popup(self, title, message):
+        popup = Popup(title=title,
+                      content=Label(text=message),
+                      size_hint=(None, None), size=(400, 400))
+        popup.open()
+
+    def get_download_path(self):
+        return filechooser.get_downloads_dir()
+
     def download_video(self, instance):
 
         download_thread = Thread(target=self.raw_download)
         download_thread.start()
 
     def raw_download(self):
-        url = self.url_input.text
-        video = self.c.get(url)
-        path = "/storage/emulated/0/"
-        quality = Quality.BEST
         try:
+            url = self.url_input.text
+            video = self.c.get(url)
+            Clock.schedule_once(lambda dt: self.show_popup("Success", "URL erfolgreich geladen!"))
+
+            path = self.get_download_path()
+            quality = Quality.BEST
             video.download(path=path, quality=quality, callback=self.update_progress)
+            Clock.schedule_once(lambda dt: self.show_popup("Success", "Video erfolgreich heruntergeladen!"))
         except Exception as e:
-            self.url_input.hint_text = str(e)
+            Clock.schedule_once(lambda dt: self.show_popup("Error", str(e)))
 
     def update_progress(self, pos, total):
         percentage_complete = (pos / total) * 100

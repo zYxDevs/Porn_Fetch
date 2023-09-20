1df058373af6eae397d8c1ab40c76ec74951777e
EchterAlsFake <EchterAlsFake@proton.me>
2023-09-20 07:37:39 +0200

    Trying to do a useful thing

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from kivy.uix.progressbar import ProgressBar
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from threading import Thread
-
+from plyer import filechooser
 try:
     import phub
 
-    text = "Sucessfully imported PHUB"
+    text = """
+Successfully imported PHUB. You can copy / paste the URL in this little input field here and click
+on the Download button, and if you are lucky enough, then you see the progressbar doing something ;) 
+"""
     exception = False
 
 except Exception as e:
@@ -24,12 +27,14 @@ class MyBoxLayout(BoxLayout):
     def __init__(self, **kwargs):
         super(MyBoxLayout, self).__init__(**kwargs)
         self.orientation = 'vertical'
+        self.chosen_path = None
+        self.path_set = False
 
         if exception:
-            self.url_input = TextInput(hint_text=str(text), multiline=False)
+            self.url_input = TextInput(hint_text=str(text), multiline=True)
 
         else:
-            self.url_input = TextInput(hint_text=str(text), multiline=False)
+            self.url_input = TextInput(hint_text=str(text), multiline=True)
 
         self.add_widget(self.url_input)
 
@@ -46,6 +51,12 @@ class MyBoxLayout(BoxLayout):
                       size_hint=(None, None), size=(400, 400))
         popup.open()
 
+    def open_filechooser(self, instance):
+        path = filechooser.choose_dir(title="Select Folder")  # Opens directory chooser
+        if path:  # If a directory is chosen
+            self.chosen_path = path[0]  # filechooser returns a list; we get the first item
+            self.path_set = True
+
     def download_video(self, instance):
 
         download_thread = Thread(target=self.raw_download)
@@ -57,8 +68,12 @@ class MyBoxLayout(BoxLayout):
             url = self.url_input.text
             video = c.get(url)
             Clock.schedule_once(lambda dt: self.show_popup("Success", "URL erfolgreich geladen!"))
+            if not self.path_set:
+                self.open_filechooser()
 
-            path = "/storage/emulated/0/Download/"
+            else:
+                path = self.chosen_path
+
             quality = phub.Quality.BEST
             video.download(path=path, quality=quality, callback=self.update_progress)
             Clock.schedule_once(lambda dt: self.show_popup("Success", "Video erfolgreich heruntergeladen!"))

e924a0fd1245c601282dbdab295a2b95b1a0ae99
EchterAlsFake <EchterAlsFake@proton.me>
2023-09-25 14:06:46 +0200

    Trying the file chooser stuff again.
    But Now I'll use the documentation and not ChatGPT.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
 import sys
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from plyer import filechooser
+
 
 
 try:
@@ -9,7 +15,20 @@ except Exception as e:
     exception = e
 
 
+class MainApp(MDApp):
+
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return self.choose_file()
+
+    def choose_file(self):
+        filechooser.open_file(on_selection=self.selected)
+
+    def selected(self, selection):
+        print(selection)
 
 
 
 
+MainApp().run()

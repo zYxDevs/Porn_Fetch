35e8822834f4a71f87f314e0ca52010bad3d7c33
EchterAlsFake <EchterAlsFake@proton.me>
2023-09-27 23:25:47 +0200

    It finally worked. Now I can try the download and it that works I am fucking done with it.

--- main.py
+++ main.py
@@ -1,51 +1,80 @@
 from kivy.lang import Builder
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDFlatButton
 from kivymd.uix.filemanager import MDFileManager
+from kivymd.uix.textfield import MDTextField
+from kivy.uix.progressbar import ProgressBar
+from threading import Thread
+from kivy.core.clipboard import Clipboard
 import os
+from phub import Client, Quality
 
-KV = """
+KV = '''
 BoxLayout:
     orientation: 'vertical'
-    padding: '10dp'
-    spacing: '10dp'
 
-    MDRaisedButton:
-        text: "Open File Dialog"
-        on_release: app.open_file_dialog()
-"""
+    BoxLayout:
+        MDLabel:
+            text: "Enter URL:"
+        MDTextField:
+            id: url_input
+        MDFlatButton:
+            text: "Paste"
+            on_release: app.paste_clipboard()
+
+    BoxLayout:
+        MDLabel:
+            text: "Output Location:"
+        MDTextField:
+            id: output_input
+        MDFlatButton:
+            text: "Set Path"
+            on_release: app.file_manager_open()
+
+    MDFlatButton:
+        text: "Download"
+        on_release: app.start_download_thread()
+
+    ProgressBar:
+        id: progress_bar
+        max: 100
+'''
 
 
-class FileDialogApp(MDApp):
+class DownloadApp(MDApp):
     def build(self):
-        self.file_manager = MDFileManager(
-            exit_manager=self.exit_manager,
-            select_path=self.select_path,
-        )
         return Builder.load_string(KV)
 
-    def open_file_dialog(self):
-        # Open the file manager in the main thread
-        self.file_manager.show('/')  # Show file manager from the root directory
+    def paste_clipboard(self):
+        self.root.ids.url_input.text = Clipboard.paste()
+
+    def file_manager_open(self):
+        home_dir = os.path.expanduser('~')
+        self.file_manager = MDFileManager(exit_manager=self.exit_manager,
+                                          select_path=self.select_path)
+        self.file_manager.show(home_dir)
 
     def select_path(self, path):
-        # Callback for when a file/directory is selected
-        directory = os.path.dirname(path)
-        popup = Popup(title='Selected File and Directory',
-                      content=Label(text=f"File: {path}\nDirectory: {directory}"),
-                      size_hint=(None, None), size=(400, 400))
-        popup.open()
         self.exit_manager()
+        self.root.ids.output_input.text = path
 
     def exit_manager(self, *args):
-        # Close the file manager
         self.file_manager.close()
+
+    def start_download_thread(self):
+        download_thread = Thread(target=self.download)
+        download_thread.start()
+
+    def download(self):
+        # Assuming you have the Client class and related methods defined somewhere
+        cl = Client()
+        video = cl.get(self.root.ids.url_input.text)
+        video.download(quality=Quality.BEST, path=self.root.ids.output_input.text, display=self.update_progress)
+
+    def update_progress(self, pos, total):
+        self.root.ids.progress_bar.value = (pos / total) * 100
 
 
 if __name__ == '__main__':
-    FileDialogApp().run()
-
-
-## I am jumping from the bridge if that shit doesn't work... (Joke)
+    DownloadApp().run()

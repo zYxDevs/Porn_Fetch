3c8adc5ff5bb0171232b52140adb496787036b82
EchterAlsFake <EchterAlsFake@proton.me>
2023-09-27 13:14:19 +0200

    Trying a first Download now

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivymd.uix.progressbar import MDProgressBar
 from kivy.clock import Clock
 from plyer import filechooser
 from phub import Quality, Client
+from kivy.core.clipboard import Clipboard
 # Make sure to import other necessary modules and classes like Client, Quality, and filechooser
 
 class DownloadApp(MDApp):
@@ -23,12 +24,20 @@ class DownloadApp(MDApp):
         # Creating a Button
         download_button = MDRaisedButton(text="Download", on_release=self.start_download_thread)
         self.progress_bar = MDProgressBar(value=0, max=100)
+        clipboard_button = MDRaisedButton(text="Paste from Clipboard", on_release=self.paste_from_clipboard)
+
         # Adding widgets to the main layout
         layout.add_widget(h_layout)
         layout.add_widget(download_button)
+        layout.add_widget(clipboard_button)
         layout.add_widget(self.progress_bar)
 
         return layout
+
+    def paste_from_clipboard(self, instance):
+        # Get the content from the clipboard and set it as the text of the TextField
+        clipboard_content = Clipboard.get()
+        self.url_input.text = clipboard_content
 
     def start_download_thread(self, instance):
         # Start the download in a new thread
@@ -38,12 +47,11 @@ class DownloadApp(MDApp):
     def download(self):
         # Get the URL from the TextField
         url = self.url_input.text
-        print(f"Downloading from {url}")
+        self.choose_file()
         # You can add the logic to download from the URL here
         cl = Client(language="en")
         video = cl.get(str(url))
         quality = Quality.BEST
-        self.choose_file()
         path = self.path[0]
         video.download(path=f"/{path}", quality=quality, display=self.update_progress)
 
